7933eaff1933c6ab0e6abdcc65ea5fbe09615292
jayiyer <jay.iyer@berkeley.edu>
2016-01-26 20:11:55 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,40 @@
 # Part 1
 
 def sum arr
-  return 0
+  if arr.empty?
+    return 0
+  end
+  result = 0
+  for i in 0..arr.length-1
+    result += arr[i]
+  end
+  return result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  end
+  if arr.length == 1
+    return arr[0]
+  end
+  max_value = arr.sort[-1]
+  second_value = arr.sort[-2]
+  result = max_value + second_value
+  return result
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    if n != 0
+      return false
+    end
+    return true
+  end
+  if arr.length == 1
+    return false
+  end
+  !!arr.combination(2).detect{|x, y| x + y == n}
 end
 
 # Part 2
