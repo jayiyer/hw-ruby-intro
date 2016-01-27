612ab28aca0da4f0a873594fbf20ebec65c88e42
jayiyer <jay.iyer@berkeley.edu>
2016-01-27 07:58:48 +0000

    finished part 2 (except one test on starts_with_consonant)

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -42,19 +42,55 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}" 
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  #puts "s[0] is not empty"
+  if ["A", "E", "I", "O", "U"].include?(s[0])
+    return false
+  end
+  #puts "s[0] is not an uppercase vowel"
+  if ["a", "e", "i", "o", "u"].include?(s[0])
+    return false
+  end
+  #puts "s[0] is not a lowercase vowel"
+  lowercase_all_letters = ('a'..'z').to_a
+  uppercase_all_letters = ('A'..'Z').to_a 
+  if !(lowercase_all_letters.include?(s[0])) || !(uppercase_all_letters.include?(s[0]))
+    return false
+  end
+  #puts "s[0] is a consonant"
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty? or s =~ /[^01]/
+    return false
+  end
+  str_to_int = s.to_i(2)
+  if str_to_int % 4 == 0
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  @isbn, @price
+  
+  def initialize(isbn, price)
+    raise ArgumentError, "ISBN cannot be empty" if isbn.empty?
+    raise ArgumentError, "Price must be positive" if price <= 0
+    @isbn  = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    price_str = @price.to_s
+    price_str = price_str.number_to_currency
 end
